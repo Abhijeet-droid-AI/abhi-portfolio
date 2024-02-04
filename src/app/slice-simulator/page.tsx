8fc67bb1b6afe99cfccf2a8e7a498cf719290abb
Abhijeet Dass <abhijeetdas2002@gmai.com>
2024-02-04 17:05:38 +0530

    Final Commit with major updates

---
+++ src/app/slice-simulator/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import {SliceSimulator} from '@slicemachine/adapter-next/simulator';
+import {SliceZone} from '@prismicio/react';
+
+import {components} from '../../slices';
+
+export default function SliceSimulatorPage() {
+	return (
+		<SliceSimulator
+			background="#431407"
+			sliceZone={(props) => (
+				<SliceZone {...props} components={components} />
+			)}
+		/>
+	);
+}
